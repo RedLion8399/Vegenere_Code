f1d3fcb744b6c29adaa69d7be5997a59b073bfd1
Paul Jonas <Paul.Jonas@dohle-net.de>
2023-09-28 15:54:31 +0200

    Function Letter_value added

---
+++ Functions.py
@@ -0,0 +1,54 @@
+def Letter_value(letter):
+    if letter == "A" or "a":
+        letter_velue = 0
+    elif letter == "B" or "b":
+        letter_velue = 1
+    elif letter == "C" or "c":
+        letter_velue = 2
+    elif letter == "D" or "d":
+        letter_velue = 3
+    elif letter == "E" or "e":
+        letter_velue = 4
+    elif letter == "F" or "f":
+        letter_velue = 5
+    elif letter == "G" or "g":
+        letter_velue = 6
+    elif letter == "H" or "h":
+        letter_velue = 7
+    elif letter == "I" or "i":
+        letter_velue = 8
+    elif letter == "J" or "j":
+        letter_velue = 9
+    elif letter == "K" or "k":
+        letter_velue = 10
+    elif letter == "L" or "l":
+        letter_velue = 11
+    elif letter == "M" or "m":
+        letter_velue = 12
+    elif letter == "N" or "n":
+        letter_velue = 13
+    elif letter == "O" or "o":
+        letter_velue = 14
+    elif letter == "P" or "p":
+        letter_velue = 15
+    elif letter == "Q" or "q":
+        letter_velue = 16
+    elif letter == "R" or "r":
+        letter_velue = 17
+    elif letter == "S" or "s":
+        letter_velue = 18
+    elif letter == "T" or "t":
+        letter_velue = 19
+    elif letter == "U" or "u":
+        letter_velue = 20
+    elif letter == "V" or "v":
+        letter_velue = 21
+    elif letter == "W" or "w":
+        letter_velue = 22
+    elif letter == "X" or "x":
+        letter_velue = 23
+    elif letter == "Y" or "y":
+        letter_velue = 24
+    elif letter == "Z" or "z":
+        letter_velue = 25
+    return letter_velue
