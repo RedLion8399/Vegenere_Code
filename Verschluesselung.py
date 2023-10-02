749747f3e2309ae98fc6f9eeb82edd952b84fbf3
Paul Jonas <Paul.Jonas@dohle-net.de>
2023-10-02 21:18:17 +0200

    last changes undone

--- Verschluesselung.py
+++ Verschluesselung.py
@@ -1,30 +1,27 @@
 from Functions import decode, endcode, letter_value, value_letter
 
-code = []
-message = 0
-key = 0
-k_i = 0         # k_i ist der Key index
-
-while message == 0:
+while True:
     try:
         message = input("Nachricht:").lower()
         mes_len = len(message)
         mes_code = decode(message, mes_len)
         key_input = True
+        break
     except KeyError:
-        message = 0
         print("Bitte geben sie nur Buchstaben des lateinischen Alphabets ein.") 
 
-while key == 0:
+while True:
     try:
         key = input("Schlüssenwort:").lower()
         key_len = len(key)
         key_code = decode(key, key_len)
         mes_input = True
+        break
     except KeyError:
-        key = 0
         print("Bitte geben sie nur Buchstaben des lateinischen Alphabets ein.") 
 
+k_i = 0
+code = []
 for m_i in range(mes_len):
     if mes_code[m_i] == " ":
         code.append(" ")
