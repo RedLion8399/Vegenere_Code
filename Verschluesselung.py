46f9226d7d77de659aa07ce905426cd4ba56893a
Paul Jonas <Paul.Jonas@dohle-net.de>
2024-11-24 15:46:25 +0100

    feat(): Centralize in Text class
    Short access files
    Move external functions into Text class

--- Verschluesselung.py
+++ Verschluesselung.py
@@ -1,22 +1,8 @@
-from Functions import Text, decrypt
+from Functions import Text
 
 message: Text = Text("Nachricht: ")
 key: Text = Text("Schlüsselwort: ")
 
-k_i = 0
-code: list[int | str] = []
+message.encrypt(key)
 
-for m_i in range(message.len):
-    if message.code[m_i] == " ":
-        code.append(" ")
-    else:
-        value = message.code[m_i] + key.code[k_i]
-        if k_i + 1 < key.len:
-            k_i += 1
-        elif k_i + 1 == key.len:
-            k_i = 0
-        if value > 25:
-            value -= 26
-        code.append(value)
-
-print(decrypt(code, len(code)))
+print(message.convert_to_letters())
